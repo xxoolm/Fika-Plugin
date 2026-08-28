d04d485db746ff0911d0ec27bb5ee97f19cd75fc
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-08-28 21:29:11 +0200

    Revert "Disable SPT patch"
    This reverts commit d128d1d8f01de4fdf62219b3ec1076723a5f0780.

--- Fika.Core/FikaPlugin.cs
+++ Fika.Core/FikaPlugin.cs
@@ -358,7 +358,6 @@ public sealed class FikaPlugin : BaseUnityPlugin
         new SendPlayerScavProfileToServerAfterRaidPatch().Disable();
         new MatchStartServerLocationPatch().Disable();
         new QuestAchievementRewardInRaidPatch().Disable();
-        new OverrideMaxAiAliveInRaidValuePatch().Disable();
     }
 
     public enum EPingSound
